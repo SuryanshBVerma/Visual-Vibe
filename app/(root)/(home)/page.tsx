ce3dfbe71d571e0a029e00dc03461360d570bdf2
“SuryanshBVerma-christuniversity” <suryansh.bachchanverma@mca.christuniversity.in>
2024-07-10 07:39:06 +0530

    setup Clerk Auth

---
+++ app/(root)/(home)/page.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <section className='flex size-full flex-col gap-10 text-white'>
+      <h1 className='text-3xl font-bold '>
+        <div className='h-[300px] w-full rounded-[20px] bg-hero bg-cover'>
+
+          <div className='flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11'>
+            <h1 className='glassmorphism'>Upcoming meeting at : 12:30 PM</h1>
+          </div>
+
+        </div>
+      </h1>
+    </section>
+  )
+}
+
+export default Home
